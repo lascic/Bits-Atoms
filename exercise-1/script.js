6839cc272c51086c2ecaabe3ad613de8bdfe7c72
Lukman Ascic <git@lukmanascic.ch>
2022-05-19 16:31:09 +0200

    Set up animation on reload

---
+++ exercise-1/script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById("animation"),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    path: "resources/flora-app-icon-animation.json"
+});
